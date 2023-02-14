53cb4f87e3d6cb131bbea5ff3423ecad2f57bce3
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-02-13 22:17:48 -0800

    activity

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+         
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//* JavaScript module syntax to import useState from React
+// import React from 'react'
+// import { useState } from 'react'
+
+//* vs one line of code vvv
+import React, { useState } from 'react'
+
+function App() {
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
+    
+  )
+}
+
+export default App;
